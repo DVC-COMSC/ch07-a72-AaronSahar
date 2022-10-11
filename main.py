5bee6d87d4d96a5f8e49ade0734e4b7a1c8c9d82
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:26:40 -0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
-
-# ******************************
-# Make your Code
-# ******************************
-
-# print ('True') or print ('False')
+num1 = list(map(int, input("Enter the numbers ").split()))
+num2 = list(map(int, input("Enter the numbers ").split()))
+match = False
+for i in range (len(num2)):
+    for j in range (len(num1)):
+        if num2[i] == num1[j]:
+            match = True
+            break
+        else:
+            match = False
+    if match == False:
+        print(match)
+if match == True:
+    print(match)
